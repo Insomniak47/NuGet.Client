36b8cdd29e3fc128ec0b9b245797fb3cb3491be0
Fernando Aguilar <feaguila@microsoft.com>
2021-04-14 19:41:20 -0700

    Used all cancellationTokens in parameters

--- src/NuGet.Clients/NuGet.SolutionRestoreManager/SolutionRestoreWorker.cs
+++ src/NuGet.Clients/NuGet.SolutionRestoreManager/SolutionRestoreWorker.cs
@@ -697,7 +697,7 @@ namespace NuGet.SolutionRestoreManager
 
             var continuation = joinableTask
                 .Task
-                .ContinueWith(t => restoreOperation.ContinuationAction(t, JoinableTaskFactory));
+                .ContinueWith(t => restoreOperation.ContinuationAction(t));
 
             return await joinableTask;
         }
@@ -786,7 +786,7 @@ namespace NuGet.SolutionRestoreManager
 
             public System.Runtime.CompilerServices.TaskAwaiter<bool> GetAwaiter() => Task.GetAwaiter();
 
-            public void ContinuationAction(Task<bool> targetTask, JoinableTaskFactory jtf)
+            public void ContinuationAction(Task<bool> targetTask)
             {
                 Assumes.True(targetTask.IsCompleted);
 
